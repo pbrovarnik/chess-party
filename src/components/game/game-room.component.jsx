e1fd3dcc2238075934067b698a72d7a848c4fed1
Pasha Brovarnik <pasha@Pashas-MacBook-Pro.local>
2021-03-18 22:23:19 -0700

    removing unnecessary code to fix error

--- src/components/game/game-room.component.jsx
+++ src/components/game/game-room.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import WaitingPage from '../waiting-page/waiting-page.component';
 import Board from '../board/board.component';
