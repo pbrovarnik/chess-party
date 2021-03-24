299be60d4f834481df124dc4d6f1cb44523c94cc
Pasha Brovarnik <pasha@Pashas-MacBook-Pro.local>
2021-03-24 16:34:15 -0700

    testing out cors

--- src/socket-connections/sockets.js
+++ src/socket-connections/sockets.js
@@ -6,7 +6,10 @@ const SOCKET_URL = process.env.REACT_APP_SERVER_URL || 'http://127.0.0.1:5000';
 let socket;
 
 export const initializeSocket = () => {
-	socket = io.connect(SOCKET_URL);
+	socket = io.connect(SOCKET_URL, {
+		transports: ['websocket'],
+	});
+
 	return socket;
 };
 
