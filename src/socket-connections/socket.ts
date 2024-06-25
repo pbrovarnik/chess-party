fb6b6cb00f04bcf2668c68d30a14b7203f219f19
Pasha <pasha@getinkworks.com>
2024-06-25 16:31:31 -0700

    removed unneeded file

--- src/socket-connections/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const SOCKET_URL = `${import.meta.env.APP_SERVER_URL}/chess-party`;
-
-export const socket = io(SOCKET_URL, {
-	autoConnect: false,
-	transports: ['websocket'],
-});
