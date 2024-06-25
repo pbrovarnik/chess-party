3fc58fb8caa3fb04b1224b18dd5f786da064955f
Pasha <pasha@getinkworks.com>
2024-06-25 16:34:41 -0700

    testing deploy secrets

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -1,11 +1,9 @@
 import { useEffect, useRef } from 'react';
 import io, { Socket } from 'socket.io-client';
 
-const SOCKET_URL = `${import.meta.env.APP_SERVER_URL}/chess-party`;
-
 export const useSocket = (): Socket => {
-	console.info('SOCKET_URL_@', SOCKET_URL);
-	const { current: socket } = useRef(io(SOCKET_URL, { reconnectionAttempts: 5, reconnectionDelay: 5000, autoConnect: false, transports: ['websocket'] }));
+	console.info('SOCKET_URL_@$', `${import.meta.env.APP_SERVER_URL}/chess-party`);
+	const { current: socket } = useRef(io(`${import.meta.env.APP_SERVER_URL}/chess-party`, { reconnectionAttempts: 5, reconnectionDelay: 5000, autoConnect: false, transports: ['websocket'] }));
 
 	useEffect(() => {
 		return () => {
