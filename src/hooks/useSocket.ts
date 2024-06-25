6d451f35fb018a1b45e46e2848c18db2b68a2951
Pasha <pasha@getinkworks.com>
2024-06-25 15:52:15 -0700

    text socket url

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -4,6 +4,7 @@ import io, { Socket } from 'socket.io-client';
 const SOCKET_URL = `${import.meta.env.APP_SERVER_URL}/chess-party`;
 
 export const useSocket = (): Socket => {
+	console.info('SOCKET_URL_@', SOCKET_URL);
 	const { current: socket } = useRef(io(SOCKET_URL, { reconnectionAttempts: 5, reconnectionDelay: 5000, autoConnect: false, transports: ['websocket'] }));
 
 	useEffect(() => {
